fb9ac9db2228a153baf35d15105f0a9f528515da
tg <eat.some.trouble@gmail.com>
2025-02-24 00:23:42 +0300

    rolled back user model

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -2,11 +2,5 @@ export interface User {
   id: string;
   username: string;
   token: string;
-  role: Role;
-}
-
-export enum Role {
-  USER = 'ROLE_USER',
-  ADMIN = 'ROLE_ADMIN',
-  MODERATOR = 'ROLE_MODERATOR'
+  role: string;
 }
